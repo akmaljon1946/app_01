4b01a27645d453eda052614addd64e95487aba8a
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-05-25 18:52:33 +0500

    add react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="container">
+           	<header className="header">
+				<a href="./index.html" class="logo-link">
+					<img className="header-logo" src="./images/LOGO.svg" width="69" height="69" alt="firma logotipi" />
+				</a>
+					<div className="header-inner">
+						<ul className="header-list">
+							<li classNames="header-item">
+								<a className="header-link" href="./index.html" >
+									<span className="link-text">Home</span>
+								</a>
+							</li>
+							<li className="header-item">
+								<a className="header-link" href="#about" >About</a>
+							</li>
+							<li className="header-item">
+								<a className="header-link" href="#work" >Services</a>
+							</li>
+							<li className="header-item">
+								<a className="header-link" href="#customers" >Reviews</a>
+							</li>
+						</ul>
+						<a className="header-get" href="#" >Get Started </a>
+					</div>
+          </header>
+        </div>
     </div>
   );
 }
